c819eb4ca22c92abe72e5b2b13580fb64dd0014a
Andy Tsai <bbandydd@gmail.com>
2017-07-28 13:01:40 +0800

    3. Grid System - Container, Row, Col

--- src/containers/Home/components/Content.js
+++ src/containers/Home/components/Content.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
-import { Button } from 'reactstrap';
+import { Button, Container, Row, Col } from 'reactstrap';
 
 export default class Content extends Component {
   render() {
     return (
       <div className="content">
-        <h1>Hello, We are ReactMaker!</h1>
-        <h2>This is Content Component!</h2>
-        <Button color="danger">danger</Button>
+        <Container>
+          <Row>
+            <Col xs="12" md="4"><Button color="danger" block>Danger</Button></Col>
+            <Col xs="12" md="4"><Button color="danger" block>Danger</Button></Col>
+            <Col xs="12" md="4"><Button color="danger" block>Danger</Button></Col>
+            <Col xs="12" md="4"><Button color="danger" block>Danger</Button></Col>
+            <Col xs="12" md="4"><Button color="danger" block>Danger</Button></Col>
+            <Col xs="12" md="4"><Button color="danger" block>Danger</Button></Col>
+            <Col xs="12" md="4"><Button color="danger" block>Danger</Button></Col>
+            <Col xs="12" md="4"><Button color="danger" block>Danger</Button></Col>
+          </Row>
+        </Container>
       </div>
     );
   }
